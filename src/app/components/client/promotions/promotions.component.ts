06d3403ac0504c2cb16cfa3a4f8f287526a71457
Sebastian Ramirez <89022108+SebastianRamirez-16@users.noreply.github.com>
2023-09-07 10:21:54 -0500

    feat(view promotion) add cards of promotion

--- src/app/components/client/promotions/promotions.component.ts
+++ src/app/components/client/promotions/promotions.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromotionsService } from 'src/app/core/services/promotions/promotions.service';
 
 @Component({
   selector: 'app-promotions',
   templateUrl: './promotions.component.html',
   styleUrls: ['./promotions.component.scss']
 })
-export class PromotionsComponent {
+export class PromotionsComponent implements OnInit{
 
+  promotions: Promotion[] = [];
+
+  constructor(private promotionsService: PromotionsService) {}
+
+  ngOnInit() {
+    this.promotionsService.getPromotions().subscribe((data) => {
+      this.promotions = data;
+    });
+  }
+
+
+}
+
+interface Promotion {
+  name: string;
+  description: string;
+  initDate: string;
+  endDate: string;
+  business: {
+    name: string;
+  };
 }
