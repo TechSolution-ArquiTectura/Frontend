d9669b48db298d87ecb575cf316a9043d5b486f6
Sebastian Ramirez <u20201c031@upc.edu.pe>
2023-09-08 17:57:57 -0500

    feat(view promotion) add styles of the cards

--- src/app/components/client/promotions/promotions.component.ts
+++ src/app/components/client/promotions/promotions.component.ts
@@ -6,19 +6,49 @@ import { PromotionsService } from 'src/app/core/services/promotions/promotions.s
   templateUrl: './promotions.component.html',
   styleUrls: ['./promotions.component.scss']
 })
-export class PromotionsComponent implements OnInit{
+export class PromotionsComponent implements OnInit {
 
   promotions: Promotion[] = [];
+  promotionRows: Promotion[][] = []; // Agregamos esta propiedad
 
   constructor(private promotionsService: PromotionsService) {}
 
   ngOnInit() {
     this.promotionsService.getPromotions().subscribe((data) => {
       this.promotions = data;
+      this.updatePromotionRows(); // Llamamos a esta función para inicializar la cuadrícula
     });
   }
 
+  currentIndex = 0;
 
+  prevPage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updatePromotionRows();
+    }
+  }
+
+  nextPage() {
+    if (this.currentIndex < Math.ceil(this.promotions.length / 4) - 1) {
+      this.currentIndex++;
+      this.updatePromotionRows();
+    }
+  }
+
+  updatePromotionRows() {
+    const startIdx = this.currentIndex * 4;
+    const endIdx = startIdx + 4;
+    this.promotionRows = this.chunkArray(this.promotions.slice(startIdx, endIdx), 2);
+  }
+
+  chunkArray(array: Promotion[], size: number): Promotion[][] {
+    const chunkedArray = [];
+    for (let i = 0; i < array.length; i += size) {
+      chunkedArray.push(array.slice(i, i + size));
+    }
+    return chunkedArray;
+  }
 }
 
 interface Promotion {
