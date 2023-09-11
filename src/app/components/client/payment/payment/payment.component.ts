5a9146af58a78107f9183e1ab020de7911f3fd0f
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-09-11 15:12:51 -0500

    feat: Creation of Payment component

---
+++ src/app/components/client/payment/payment/payment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InformationComponent } from '../Components/information/information.component';
+import { PaymentFormComponent } from '../Components/payment-form/payment-form.component';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.scss']
+})
+export class PaymentComponent {
+
+}
