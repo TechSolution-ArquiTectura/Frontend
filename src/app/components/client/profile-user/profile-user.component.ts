e387523dad9970a8ca3b9db697f43df2ab6d6278
Andres (UPC) <u202116113@upc.edu.pe>
2023-11-24 00:41:17 -0500

    fix: calling the api service using the userId to obtain the user information and display it on the screen

--- src/app/components/client/profile-user/profile-user.component.ts
+++ src/app/components/client/profile-user/profile-user.component.ts
@@ -1,27 +1,35 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Cinephile } from 'src/app/core/models/cinephile.model';
 import { Group } from 'src/app/core/models/group.model';
 import { CinephileProfileService } from 'src/app/core/services/auth/cinephile/cinephile-profile.service';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProfileModalComponent } from './edit-profile-modal/edit-profile-modal.component';
+import {Review} from "../../../core/models/review.models";
+import {Gender, User} from "../../../core/models/users.model";
 
 @Component({
   selector: 'app-profile-user',
   templateUrl: './profile-user.component.html',
   styleUrls: ['./profile-user.component.scss']
 })
-export class ProfileUserComponent {
+export class ProfileUserComponent implements OnInit{
   @Input() dataEntrante:any;
   @Input() groups: any;
 
   cinemas: [] = [];
-
+  user: User = {
+    id: 0,
+    name: '',
+    email: '',
+    lastname: '',
+    birthdate: '',
+    imageSrc: '',
+    phoneNumber: '',
+  }
   @Input()
       public cinephile:Cinephile[]=[];
 
-      constructor(public dialog: MatDialog) {}
-
-      
+      constructor(public dialog: MatDialog, private _userService: CinephileProfileService) {}
 
       openEditModal(): void {
         const dialogRef = this.dialog.open(EditProfileModalComponent, {
@@ -29,8 +37,8 @@ export class ProfileUserComponent {
           height: '70%', // Ajusta al 70% del alto de la pantalla
           data: this.dataEntrante
         });
-        
-    
+
+
         dialogRef.afterClosed().subscribe(result => {
           if (result) {
             this.dataEntrante = result;
@@ -38,7 +46,14 @@ export class ProfileUserComponent {
           }
         });
       }
-  
+  ngOnInit(): void {
+    this.user.id = JSON.parse(localStorage.getItem("userId") || '{}');
+    this._userService.getPersonById(this.user.id).subscribe((data) => {
+      this.user = data;
+      console.log(this.user);
+      this.dataEntrante = this.user;
+    });
+  }
 
 }
 
