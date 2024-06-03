7437f86c842a5676e4ac53d39aaf7c51869de49b
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2024-06-03 18:43:03 -0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/TuCine/v1'
+    apiUrl: 'https://tucineapirest.azurewebsites.net/api/TuCine/v1'
   };
   
